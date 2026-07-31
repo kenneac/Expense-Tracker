b05ce5e2caca80a6135a91f73076216ade26fec8
kenneac <iamkenneac@gmail.com>
2026-07-31 16:02:38 +0100

    low bug fixes

--- mobile/src/constants/api.js
+++ mobile/src/constants/api.js
@@ -1,7 +1,6 @@
-// import { Platform } from 'react-native';
+import { Platform } from "react-native";
 
-// export const API_URL = Platform.select({
-//   web: 'https://expense-tracker-sj7n.onrender.com/api',
-//   default: 'https://expense-tracker-sj7n.onrender.com/api',
-// });
-export default  API_URL = "https://expense-tracker-sj7n.onrender.com/api"
+export const API_URL = Platform.select({
+  web: "https://expense-tracker-sj7n.onrender.com/api",
+  default: "https://expense-tracker-sj7n.onrender.com/api",
+});
